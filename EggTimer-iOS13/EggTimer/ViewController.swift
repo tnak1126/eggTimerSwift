b5c1fea59c8169a5caac08c21cce984b0d4237f0
tnak1126 <64865857+tnak1126@users.noreply.github.com>
2020-09-09 08:27:03 -0700

    Add files via upload

---
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer!
+
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var titleLable: UILabel!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 5]
+       
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLable.text = hardness
+        
+         
+        timer =  Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+  
+    }
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+           secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+            print(Float(secondsPassed) / Float(totalTime))
+
+         
+        }
+        else {
+            timer.invalidate()
+ 
+            titleLable.text = "Done!"
+            
+            func playSound(soundName: String) {
+                          let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                             player = try! AVAudioPlayer(contentsOf: url!)
+                   player.play()
+                     
+                           }
+                 playSound(soundName: "alarm_sound")
+                   print("Done! Done! Done! Done!")
+            
+        }
+  
+        }
+    }
+
+    
+
+
